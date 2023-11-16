921d382bd7ccafaf98184810c4601d9ea13fb315
David Peconka <xpecon00@vutbr.cz>
2023-11-16 09:56:17 +0100

    commit

--- cv7/cv7/Core/Src/main.c
+++ cv7/cv7/Core/Src/main.c
@@ -24,7 +24,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "lis2dw12_reg.h"
-#include "stdio.h"
+#include <stdio.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
